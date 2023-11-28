452fab8343a9d1251872cec6d72c3f755c0e53b4
Luke Padawer <54520310+lukeslab@users.noreply.github.com>
2023-11-28 16:59:31 -0500

    added stats for a single exercise.

--- frontend/src/components/ExerciseDeckCardBack.js
+++ frontend/src/components/ExerciseDeckCardBack.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function ExerciseDeckCardBack({ exercise, isEditable, setCardIsFaceup }) {
-    const { title } = exercise
-
+    const { title, stats: { timesPerformed, highestWeight, targetsHit, targetsMissed, successRate } } = exercise
+    console.log(exercise.stats)
 
     function toggleCardSide() {
         setCardIsFaceup(true)
@@ -22,11 +22,11 @@ function ExerciseDeckCardBack({ exercise, isEditable, setCardIsFaceup }) {
             <div className="flex p-2 justify-around">
                 <div className="mb-4">
                     <h3 class="font-semibold mb-2">Other stats</h3>
-                    <p class="mb-1">Times performed:</p>
-                    <p class="mb-1">Highest weight:</p>
-                    <p class="mb-1">Targets hit:</p>
-                    <p class="mb-1">Targets Missed:</p>
-                    <p class="mb-1">Success rate:</p>
+                    <p class="mb-1">Times performed: {timesPerformed}</p>
+                    <p class="mb-1">Highest weight: {highestWeight}</p>
+                    <p class="mb-1">Targets hit: {targetsHit}</p>
+                    <p class="mb-1">Targets Missed: {targetsMissed}</p>
+                    <p class="mb-1">Success rate: {successRate}</p>
                 </div>
                 <button onClick={toggleCardSide} className="bg-blue-500 text-white px-4 py-2 flex self-start">
                     Back
